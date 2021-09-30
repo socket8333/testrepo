771d3f5cd227ea3da8e7403847598434b9e6dd00
Name <85176084+socket8333@users.noreply.github.com>
2021-09-30 14:27:41 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config() //this pulls in enviro vairalebles and puts them in config
+
+
+console.log(process.argv[2]) 
+console.log(process.argv[3]) 
+console.log(process.env.USER) 
+console.log(process.env.FOO) 
+
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+
+server.get('/hello', (req, res) => {
+  res.send('<h1>HELLO THERE!</h1>')
+})
+
+const port = process.env.PORT || 3000
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`)
+})
